b8cfde28b6889ba8229b67d6231dbbf9b214cfa7
VickyNovarese <mariavirginianovarese@gmail.com>
2026-07-10 12:09:54 +0200

    click in menu options

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -37,3 +37,28 @@ test('Left menu items in Orange',async({page})=>{
     expect (menuItemTexts).toEqual(expectedListItem)
     expect (expectedListItem[0]).toEqual('Admin')
 })
+
+test('Click in menu options', async({page})=>{
+    test.setTimeout(90000)
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.getByRole('textbox',{name:'Username'}).fill('Admin')
+    await page.getByRole('textbox',{name:'Password'}).fill('admin123')
+    await page.getByRole('button', {name:'Login'}).click()
+    await expect (page.getByRole('link', {name:'Admin'})).toBeVisible()
+
+    const leftMenuListItem= page.getByLabel('Sidepanel').getByRole('listitem')
+
+    const listItemCount = await leftMenuListItem.count()
+
+    for(let i=0;i<listItemCount;i++){
+        const menuItem = leftMenuListItem.nth(i)
+        const menuText = await menuItem.innerText()
+        console.log(menuText)
+         await menuItem.click()
+        if(menuText =='Maintenance'){
+         await page.goBack()
+          }
+    }
+
+
+    })
