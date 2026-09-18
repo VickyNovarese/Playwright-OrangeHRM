4475af3b0a9bf2dd7f4dcade9c34650380adfbe8
VickyNovarese <mariavirginianovarese@gmail.com>
2026-09-18 22:42:43 +0200

    user.spec update

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -2,15 +2,26 @@ import{test,expect} from 'playwright/test';
 import { LoginPage } from '../pageObjects/loginPage';
 import { TopBarMenu } from '../components/topBarMenu/topBarMenu';
 
+
 declare const process: {
   env: Record<string, string | undefined>
 }
+
+const username = process.env.ADMIN_USERNAME ?? ''
+const password = process.env.ADMIN_PASSWORD ?? ''
+let topBarMenu: TopBarMenu;
+
+test.beforeEach(async ({ page }) => {
+  const loginPage = new LoginPage(page)
+  await loginPage.login(username, password)
+  await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+  await page.getByRole('link', { name: 'Admin' }).click()
+  
+})
 test('Get all username', async ({page})=>{
 
-        const loginPage = new LoginPage(page)
-        await loginPage.login('Admin','admin123')
-        await expect (page.getByRole('link', {name:'Admin'})).toBeVisible()
-        await page.getByRole('link', {name:'Admin'}).click()
+        
+        
         await page.getByRole('navigation').getByText('User Management').click()
         await page.getByRole('menuitem').click()
 
@@ -26,18 +37,14 @@ test('Get all username', async ({page})=>{
                 username.push(usernameContent)
             }
         }
-        console.log(username)
+        
 
     })
 
 
     test('Get all Employee Name', async ({page})=>{
-        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        await page.getByRole('textbox',{name:'Username'}).fill('Admin')
-        await page.getByRole('textbox',{name:'Password'}).fill('admin123')
-        await page.getByRole('button', {name:'Login'}).click()
-        await expect (page.getByRole('link', {name:'Admin'})).toBeVisible()
-        await page.getByRole('link', {name:'Admin'}).click()
+        
+    
         await page.getByRole('navigation').getByText('User Management').click()
         await page.getByRole('menu').click()
 
@@ -52,21 +59,14 @@ test('Get all username', async ({page})=>{
                 employeeName.push(employeeContent)
             }
         }
-        console.log(employeeName)
-
+        
+     
     })
 
 
-    test ("Testing Menu options using POM", async({page})=>{
+    test ("Testing Menu options using POM", async({page})=>{        
         
-        const username = process.env.ADMIN_USERNAME??""
-        const password = process.env.ADMIN_PASSWORD??""
-        test.setTimeout(90_000)
-        const loginPage = new LoginPage(page)
-        await loginPage.login(username,password)
-        await expect(page.getByRole('link', {name:'Admin'})).toBeVisible()
-        await page.getByRole('link', {name:'Admin'}).click()
-        const topBarMenu = new TopBarMenu(page)      
+        const topBarMenu = new TopBarMenu(page)   
         await topBarMenu.userManagement.clickOnUsers()
         await expect (page.getByRole('heading', {name:"System Users"})).toBeVisible()
         await topBarMenu.job.clickOnJobTitleOption()
@@ -78,7 +78,13 @@ test('Get all username', async ({page})=>{
         await topBarMenu.job.clickOnJobCategoriesOption()
         await expect (page.getByRole('heading', {name:"Job Categories"})).toBeVisible()
         await topBarMenu.job.clickOnWorkShiftsOption()
-        await expect (page.getByRole('heading', {name:"Work Shifts"})).toBeVisible()        
+        await expect (page.getByRole('heading', {name:"Work Shifts"})).toBeVisible() 
+    })
+
+    test ("Testing menu options using POM 2", async({page}) =>{  
+
+
+        const topBarMenu = new TopBarMenu(page)                  
         await topBarMenu.organization.clickOnGeneralInformation()
         await expect (page.getByRole('heading', {name:"General Information"})).toBeVisible()   
         await topBarMenu.organization.clickOnLocations()
@@ -96,5 +102,5 @@ test('Get all username', async ({page})=>{
         await topBarMenu.qualifications.clickOnMembershipsOption()
         await expect (page.getByRole('heading', {name:"Memberships"})).toBeVisible()
 
-
-    })
+        })
+   
